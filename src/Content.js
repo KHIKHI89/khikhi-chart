b91fe540b27bd915168beabc15e6a5e25448ad19
Khadidja <111190473+KHIKHI89@users.noreply.github.com>
2023-06-06 19:14:14 +0100

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -3,16 +3,14 @@ import ReactDOM from "react-dom";
 import Dashboard from "./Dashboard";
 import "./style.css";
 //import Content from "./Mine";
-import Content from "./anotherMine"
+//import Content from "./anotherMine"
+import Content from "./Exemple";
 import {data} from "./donee"
 function App() {
   return (
     <div className="App">
-      <div className="App-body">
-        <section className="dashboard">
           <Content />
-        </section>
-      </div>
+        
     </div>
   );
 }
